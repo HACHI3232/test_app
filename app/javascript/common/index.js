a5f1089fa37bd41cc97db99faf0c8b777e7c5814
HACHI3232 <harada.chisato526@gmail.com>
2023-04-19 21:29:29 +0900

    add: js for index

--- app/javascript/common/index.js
+++ app/javascript/common/index.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-	var select = document.querySelector('select[name="sort_order"]');
+	var order_select = document.querySelector('select[name="sort_order"]');
+	var category_select = document.querySelector('select[name="category_id"]');
 
-	select.addEventListener("change", function () {
-		var selected_value = select.value;
+
+	order_select.addEventListener("change", function () {
+		var selected_value = order_select.value;
 
 		var xhr = new XMLHttpRequest();
 		xhr.responseType = "document";
@@ -18,4 +20,24 @@ document.addEventListener("DOMContentLoaded", function () {
 		};
 		xhr.send();
 	});
+
+	category_select.addEventListener("change", function () {
+		var selected_value = category_select.value;
+
+		var xhr = new XMLHttpRequest();
+		xhr.responseType = "document";
+		xhr.open("GET", "/posts?category_id=" + selected_value, true);
+		xhr.onload = function () {
+			var html = xhr.response.documentElement.outerHTML;
+			var parser = new DOMParser();
+			var doc = parser.parseFromString(html, "text/html");
+			var container = doc.querySelector(".contents");
+			var containerHtml = container.innerHTML;
+			const contentsDOM = document.getElementsByClassName("contents");
+			contentsDOM[0].innerHTML = containerHtml;
+		};
+		xhr.send();
+	})
+
+	
 });
