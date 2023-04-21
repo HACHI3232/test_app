09ecccd7559353c37d605c4712c1fdc368270b9b
HACHI3232 <harada.chisato526@gmail.com>
2023-04-22 01:15:30 +0900

    add: js

--- app/javascript/common/index.js
+++ app/javascript/common/index.js
@@ -1,43 +1,61 @@
 document.addEventListener("DOMContentLoaded", function () {
 	var order_select = document.querySelector('select[name="sort_order"]');
-	var category_select = document.querySelector('select[name="category_id"]');
+	var category_select = document.querySelector('select[name="category_id"]'); 
 
+	if (order_select) {
+		order_select.addEventListener("change", function () {
+			var selected_value = order_select.value;
 
-	order_select.addEventListener("change", function () {
-		var selected_value = order_select.value;
+			var xhr = new XMLHttpRequest();
+			xhr.responseType = "document";
+			xhr.open("GET", "/posts?sort_order=" + selected_value, true);
+			xhr.onload = function () {
+				var html = xhr.response.documentElement.outerHTML;
+				var parser = new DOMParser();
+				var doc = parser.parseFromString(html, "text/html");
+				var container = doc.querySelector(".contents");
+				var containerHtml = container.innerHTML;
+				const contentsDOM = document.getElementsByClassName("contents");
+				contentsDOM[0].innerHTML = containerHtml;
+			};
+			xhr.send();
+		});
+	}
 
-		var xhr = new XMLHttpRequest();
-		xhr.responseType = "document";
-		xhr.open("GET", "/posts?sort_order=" + selected_value, true);
-		xhr.onload = function () {
-			var html = xhr.response.documentElement.outerHTML;
-			var parser = new DOMParser();
-			var doc = parser.parseFromString(html, "text/html");
-			var container = doc.querySelector(".contents");
-			var containerHtml = container.innerHTML;
-			const contentsDOM = document.getElementsByClassName("contents");
-			contentsDOM[0].innerHTML = containerHtml;
-		};
-		xhr.send();
-	});
+	if (category_select) {
+		category_select.addEventListener("change", function () {
+			var selected_value = category_select.value;
 
-	category_select.addEventListener("change", function () {
-		var selected_value = category_select.value;
+			var xhr = new XMLHttpRequest();
+			xhr.responseType = "document";
+			xhr.open("GET", "/posts?category_id=" + selected_value, true);
+			xhr.onload = function () {
+				var html = xhr.response.documentElement.outerHTML;
+				var parser = new DOMParser();
+				var doc = parser.parseFromString(html, "text/html");
+				var container = doc.querySelector(".contents");
+				var containerHtml = container.innerHTML;
+				const contentsDOM = document.getElementsByClassName("contents");
+				contentsDOM[0].innerHTML = containerHtml;
+			};
+			xhr.send();
+		})
+	}
 
-		var xhr = new XMLHttpRequest();
-		xhr.responseType = "document";
-		xhr.open("GET", "/posts?category_id=" + selected_value, true);
-		xhr.onload = function () {
-			var html = xhr.response.documentElement.outerHTML;
-			var parser = new DOMParser();
-			var doc = parser.parseFromString(html, "text/html");
-			var container = doc.querySelector(".contents");
-			var containerHtml = container.innerHTML;
-			const contentsDOM = document.getElementsByClassName("contents");
-			contentsDOM[0].innerHTML = containerHtml;
-		};
-		xhr.send();
+});
+
+window.addEventListener('load', (event) => {
+
+	const menuIcon = document.getElementsByClassName("menu-icon")[0];
+	const mainNavigation = document.getElementsByClassName("hamburger__nav")[0];
+
+	menuIcon.addEventListener("click",function (e) {
+		console.log('opkopkl');
+		if (mainNavigation.style.display == "none") {
+			mainNavigation.style.display = "inline-flex";
+		} else {
+			mainNavigation.style.display = "none";
+		}
 	})
 
-	
-});
+})
