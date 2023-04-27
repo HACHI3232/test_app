129d935fb424d100edd73b2ba22ca4227e2dd0e1
HACHI3232 <harada.chisato526@gmail.com>
2023-04-27 13:15:45 +0900

    fix:render setting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ require("@rails/activestorage").start();
 require("channels");
 require("keyboard");
 require("common");
-require("keyboard/word-game");
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
