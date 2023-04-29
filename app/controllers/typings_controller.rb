ab3c0f1aceb60f50cfd5e8a464c09003cfceda4f
HACHI3232 <harada.chisato526@gmail.com>
2023-04-29 18:10:11 +0900

    fix:view

--- app/controllers/typings_controller.rb
+++ app/controllers/typings_controller.rb
@@ -7,22 +7,18 @@ class TypingsController < ApplicationController
   def edit
     @dictionaries = Post.all
     @character = Character.find_by(id: 1)
-    # @random_posts = Post.order("RAND()").limit(10)
-    @random_posts = Post.order("RANDOM()").limit(10)
+    @random_posts = Post.order("RAND()").limit(10)
+    # @random_posts = Post.order("RANDOM()").limit(10)
 
   end
 
   def result
     @character = Character.find_by(id: 1)
     @success = params[:success].to_i
-    @character.update(experience:@character.experience+@success)
-
-
-
+    @character.update(experience: @character.experience + @success)
     if @character.experience >= 20
       @character.update(level: @character.level+5, experience: @character.experience - 20)
     end
-
     @result = {
       success: params[:success],
       failed: params[:failed],
